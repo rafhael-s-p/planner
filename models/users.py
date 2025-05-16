8c8bfb16f21d3a5c784100860b2e00c9cf0b15a6
Rafhael <rafhael.dev@gmail.com>
2025-05-16 08:56:33 -0300

    feat: structuring fast api apps

---
+++ models/users.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, EmailStr
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+            }
+        }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!"
+            }
+        }
